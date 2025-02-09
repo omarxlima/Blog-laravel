4dcf7b27831ff95007bc35d6061e1ddf5e81370c
omarxlima <marxmaciel@gmail.com>
2025-02-09 17:48:48 -0300

    Add: links ativos na sidebar

--- resources/views/layouts/includes/admin-sidebar.blade.php
+++ resources/views/layouts/includes/admin-sidebar.blade.php
@@ -4,44 +4,50 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="{{ route('dashboard')}}">
+                <a class="nav-link  {{Route::currentRouteName() == 'dashboard' ? 'active' : ''}}"   href="{{ route('dashboard')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
                 {{-- CATEGRIAS --}}
                 <div class="sb-sidenav-menu-heading">Interface</div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                <a class="nav-link {{ Route::currentRouteName() == 'categoria.index' || Route::currentRouteName() == 'categoria.create' ? 'collapsed active' : 'collapsed' }}" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                     Categorias
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse {{ Route::currentRouteName() == 'categoria.index'  || Route::currentRouteName() == 'categoria.create' ? 'show' : '' }}" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{route('categoria.index')}}">Categorias</a>
-                        <a class="nav-link" href="{{route('categoria.create')}}">Adicionar Categoria</a>
+                        <a class="nav-link {{Route::currentRouteName() == 'categoria.index' ? 'active' : ''}}" href="{{route('categoria.index')}}">Categorias</a>
+                        <a class="nav-link {{Route::currentRouteName() == 'categoria.create' ? 'active' : ''}}" href="{{route('categoria.create')}}">Adicionar Categoria</a>
                     </nav>
                 </div>
 
                 {{-- POSTS --}}
 
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePosts" aria-expanded="false" aria-controls="collapsePosts">
+                <a class="nav-link {{ Route::currentRouteName() == 'posts.index'  || Route::currentRouteName() == 'posts.create' ? 'collapsed active' : 'collapsed' }}" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePosts" aria-expanded="false" aria-controls="collapsePosts">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                     Posts
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapsePosts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse {{ Route::currentRouteName() == 'posts.index'  || Route::currentRouteName() == 'posts.create' ? 'show' : '' }}" id="collapsePosts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
-                        <a class="nav-link" href="{{route('posts.create')}}">Adicionar Post</a>
+                        <a class="nav-link {{Route::currentRouteName() == 'posts.index' ? 'active' : ''}}" href="{{route('posts.index')}}">Posts</a>
+                        <a class="nav-link {{Route::currentRouteName() == 'posts.create' ? 'active' : ''}}" href="{{route('posts.create')}}">Adicionar Post</a>
                     </nav>
                 </div>
 
                 {{-- USERS --}}
 
-                <a class="nav-link" href="{{ route('users.index')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                {{-- <a class="nav-link"  {{Request::is('admin/users*') ? 'active' : ''}} href="{{ route('users.index')}}">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                    Usuários
+                </a> --}}
+
+                <a class="nav-link {{Route::currentRouteName() == 'users.index' ? 'active' : ''}}" href="{{ route('users.index') }}">
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                     Usuários
                 </a>
+                
 
                 <div class="sb-sidenav-menu-heading">Addons</div>
                 <a class="nav-link" href="charts.html">
