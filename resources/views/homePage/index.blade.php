e4cd9f9506be255b1d7e34454fe9fd855d010c14
omarxlima <marxmaciel@gmail.com>
2025-02-13 00:20:28 -0300

    Add: Metatags em Posts

--- resources/views/homePage/index.blade.php
+++ resources/views/homePage/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+
+@section('title', "Developer Lima")
+@section('meta_description', "Developer Lima")
+@section('meta_keyword',"Developer Lima")
+
 @section('content')
 
 <h4>Hello</h4>
